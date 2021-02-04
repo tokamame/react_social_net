b60e4885bb962eb25514270bfe1f3e95bdf8aa4d
Kaniet <tokamame@gmail.com>
2021-02-04 10:51:29 +0300

    split the user component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+    <img src='https://logoeps.com/wp-content/uploads/2014/09/23689-chrome-logo-icon-vector-icon-vector-eps.png' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyPosts from './MyPosts/Post/MyPosts';
+import s from './Profile.module.css';
+
+const Profile = () => {
+    return <div className={s.content}>
+    <img src='https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999943-stock-photo-chaise-lounge-and-umbrella-on.jpg' />
+
+    <div>
+      ava + description
+  </div>
+    <MyPosts />
+  </div>
+}
+
+export default Profile;
