0ea3bc901a2d4670c71c5fc93105f1842a556c34
Kaniet <tokamame@gmail.com>
2021-02-04 22:03:04 +0300

    lesson 18 react-samurai passed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,8 +3,8 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={s.content}>
-    <img src='https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999943-stock-photo-chaise-lounge-and-umbrella-on.jpg' />
+    return <div>
+    <img src='https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999943-stock-photo-chaise-lounge-and-umbrella-on.jpg' alt='profile logo' />
 
     <div>
       ava + description
