0a69545af1249a174a1a2709ea90f1f4561636c8
Kaniet <tokamame@gmail.com>
2021-02-05 10:39:00 +0300

    Lesson 20 passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
 
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs/>
+                <Route path='/profile' component={Profile} />
+                <Route path ='/dialogs' component={Dialogs} />
+
             </div>
 
-            {/*
-      <Profile />
-*/}
 
         </div>
-
+</BrowserRouter>
 
     );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return <nav className={s.nav}>
     <div className={s.item}>
-      <a href='#one'>Profile</a>
+      <NavLink to='/profile'>Profile</NavLink>
     </div>
     <div className={`${s.item} ${s.active}`}>
-      <a href='#one'>Messages</a>
+      <NavLink to='/dialogs'>Messages</NavLink>
     </div>
     <div className={s.item}>
-      <a href='#one'>News</a>
+      <NavLink to='/news'>News</NavLink>
     </div>
     <div className={s.item}>
-        <a href='#one'>Music</a>
+        <NavLink to='/music'>Music</NavLink>
     </div>
     <div className={s.item}>
-      <a href='#one'>Settings</a>
+      <NavLink to='/settings'>Settings</NavLink>
     </div>
 
   </nav>
